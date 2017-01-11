a2993be1d4f33f823a3fbc0530ab8f664a74cc6f
Edward Qiu <edwardqiu@berkeley.edu>
2017-01-11 07:49:12 +0000

    Improved the title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    MatrixCalendar
+                    MatrixCalendar.
                 </div>
             </div>
         </div>
