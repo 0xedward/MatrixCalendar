a041bec162229c1a9f74c698407f04102077a4bf
Edward Qiu <edwardqiu@berkeley.edu>
2017-01-11 07:37:26 +0000

    changes to app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Cool App Calendar</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
