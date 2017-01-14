a0948fcbbf645043c904b97f9eb0a99fa68f94c2
Kevin Bastian <kjb5642@rit.edu>
2017-01-14 06:50:00 +0000

    Finished a really hardcoded matrix

--- resources/views/matrix/matrix.blade.php
+++ resources/views/matrix/matrix.blade.php
@@ -8,7 +8,8 @@
 
 
 @section('content')
-	<h2>Eisenhower Matrix</h2>	
+</div>
+	
 	@if(!empty($tasks))
 	    <ul>
 	    @foreach($tasks as $task)
@@ -18,24 +19,25 @@
 	    @endforeach
 	    </ul>
 	@endif
-
-<table>
-        <tr>
-            <th class="smally">/</th>
-            <th>URGENT</th>
-            <th>NOT URGENT</th>
-        </tr>
-        <tr>
-            <td height="50" class="vertical_td"><div class="rotate">IMPORTANT</div></td>
-            <td style="background:#CFF09E;">Quadrant 1</td>#D5DED9
-            <td>Quadrant 2</td>
-        </tr>
-
-        <tr>
-            <td height="5"><div class="rotate">NOT IMPORTANT</div></td>
-            <td>Quadrant 1</td>
-            <td>Quadrant 2</td>
-        </tr>
-</table>
+<center>
+    <table>
+            <tr>
+                <th class="smally"> </th>
+                <th style="background:#D5DED9;">URGENT</th>
+                <th style="background:#D5DED9;">NOT URGENT</th>
+            </tr>
+            <tr>
+                <td style="background:#D5DED9;" class="vertical_td"><div class="rotate">IMPORTANT</div></td>
+                <td style="background:#CFF09E;">Quadrant 1</td>
+                <td style="background:#80aaef;">Quadrant 2</td>
+            </tr>
+    
+            <tr>
+                <td style="background:#D5DED9;" class="vertical_td"><div class="rotate">NOT IMPORTANT</div></td>
+                <td style="background:#f5fb5f;">Quadrant 3</td>
+                <td style="background:#ff7277;">Quadrant 4</td>
+            </tr>
+    </table>
+</center>
 
 @stop
